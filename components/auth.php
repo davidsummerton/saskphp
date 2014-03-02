c31dac63c6feebcea5a45e2af6c0bf2449438840
Anh Saker <anhsk.ohbo@gmail.com>
2014-03-03 01:20:55 +0700

    Update docblocks, remove close php tag, add getUserId() method

--- components/auth.php
+++ components/auth.php
@@ -2,28 +2,50 @@
 
 class Auth {
 
-	public static function login($id) {
-
-   		$_SESSION['user_id'] = $id;
-   		return true;
-
+	/**
+	 * Login the user with an ID.
+	 * 
+	 * @param  int $id
+	 * @return void
+	 */
+	public static function login($id)
+	{
+		$_SESSION['user_id'] = $id;
 	}
 
-	public static function logout() {
-
-		if(isset($_SESSION['user_id'])) {
+	/**
+	 * Logout current user.
+	 * 
+	 * @return void
+	 */
+	public static function logout()
+	{
+		if (isset($_SESSION['user_id']))
+		{
 			unset($_SESSION['user_id']);
 			session_destroy();
 		}
-
 	}
 
-	public static function loggedIn() {
-		
+	/**
+	 * Checks if the current visitor is logged-in.
+	 * 
+	 * @return boolean
+	 */
+	public static function loggedIn()
+	{
 		return isset($_SESSION['user_id']);
-		
+	}
+
+	/**
+	 * Returns the current user ID.
+	 * 
+	 * @return int|null
+	 */
+	public static function getUserId()
+	{
+		// Use static::loggedIn() if use PHP 5.3 >=
+		return self::loggedIn() ? $_SESSION['user_id'] : null;
 	}
 
 }
-
-?>
