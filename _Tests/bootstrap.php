4687ceaed6fff738e00cb175833ca1a3b6114395
sjstoelting <mail@stefanie-stoelting.de>
2014-03-03 22:04:21 +0100

    Revert "Added some PHPDoc tags and changed some formats."
    This reverts commit 3a98237828874b2415cd81148d235a7c7da028a9.

--- _Tests/bootstrap.php
+++ _Tests/bootstrap.php
@@ -1,9 +1,6 @@
 <?php
+
 /* 
  * The bootstrap contains the initialization for unit tests.
- *
- * @author Stefanie Janine Stoelting <mail@stefanie-stoelting.de>
- * @link http://saskphp.com/ Sask website
- * @license http://opensource.org/licenses/MIT MIT
  */
 
