3a98237828874b2415cd81148d235a7c7da028a9
sjstoelting <mail@stefanie-stoelting.de>
2014-03-03 22:02:35 +0100

    Added some PHPDoc tags and changed some formats.

--- _Tests/bootstrap.php
+++ _Tests/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
-
 /* 
  * The bootstrap contains the initialization for unit tests.
+ *
+ * @author Stefanie Janine Stoelting <mail@stefanie-stoelting.de>
+ * @link http://saskphp.com/ Sask website
+ * @license http://opensource.org/licenses/MIT MIT
  */
 
