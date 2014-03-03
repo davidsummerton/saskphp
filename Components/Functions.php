4707207d5d0155e988f3169765d438c18ddc9805
sjstoelting <mail@stefanie-stoelting.de>
2014-03-03 22:07:37 +0100

    Added some PHPDoc tags and changed some formats.

--- Components/Functions.php
+++ Components/Functions.php
@@ -1,30 +1,62 @@
 <?php
-
+/**
+ * Contains system functions
+ *
+ * @author David Summerton
+ * @author Stefanie Janine Stoelting <mail@stefanie-stoelting.de>
+ * @link http://saskphp.com/ Sask website
+ * @license http://opensource.org/licenses/MIT MIT
+ */
 class System
 {
 
+    /**
+     * 
+     * @return boolean
+     */
     public static function isPost()
     {
-        return $_SERVER['REQUEST_METHOD'] == 'POST';
+        return $_SERVER['REQUEST_METHOD'] === 'POST';
     }
 
+    /**
+     * 
+     * @return boolean
+     */
     public static function isGet()
     {
-        return $_SERVER['REQUEST_METHOD'] == 'GET';
+        return $_SERVER['REQUEST_METHOD'] === 'GET';
     }
 
+    /**
+     * 
+     * @param string $url
+     */
     public static function redirect($url)
     {
         header('Location: ' . $url);
         exit();
     }
 
+    /**
+     * Replaces all spaces with hyphens and some special chars.
+     * 
+     * @param string $string The string, that should be cleaned
+     * @return string The cleand string
+     */
     public static function cleanseString($string)
     {
         $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
         return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
     }
 
+    /**
+     * 
+     * @param array $data
+     * @param integer $page
+     * @param integer $per_page
+     * @return array
+     */
     public static function paginate($data, $page = 1, $per_page = 5)
     {
         $min = (($page * $per_page) - $per_page) + 1;
