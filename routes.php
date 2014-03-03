467a6ff3ab196948c46515f9e22454139f2deb42
sjstoelting <mail@stefanie-stoelting.de>
2014-03-03 22:19:14 +0100

    Revert "Added some PHPDoc tags and changed some formats."
    This reverts commit e07e8e5c97683a88c2c551d5190cb2d1ebf09845.

--- routes.php
+++ routes.php
@@ -1,22 +1,20 @@
 <?php
-/**
- * Initialize the router and set routes<br>
- * ==================<br>
- * New routes are added like this:<br>
- * $router->add('/uri', 'Class/function');<br>
- * To accept parameters in the URI you can use two options:<br>
- * :s = string<br>
- * :i = integer<br>
- * Parameter options are used like so:<br>
- * $router->add('/uri/:s/:i', 'Class/function');<br>
- * Integers will still be accepted if you use :s and you can just convert them 
- * to a true integer later if you wish.
+
+/*
+ * 	Initialize the router and set routes
+ * 	==================
+ * 	New routes are added like this:
+ * 	$router->add('/uri', 'Class/function');
  *
- * @author David Summerton
- * @link http://saskphp.com/ Sask website
- * @license http://opensource.org/licenses/MIT MIT
+ * 	To accept parameters in the URI you can use two options:
+ * 	:s = string
+ * 	:i = integer
  *
- * 
+ * 	Parameter options are used like so:
+ * 	$router->add('/uri/:s/:i', 'Class/function');
+ *
+ * 	Integers will still be accepted if you use :s and you can
+ * 	just convert them to a true integer later if you wish.
  */
 
 $router = new Router();
