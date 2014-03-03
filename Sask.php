97ba61c7c27c00e83ef8d4f9228471e3a33600dc
sjstoelting <mail@stefanie-stoelting.de>
2014-03-03 21:57:35 +0100

    Added some PHPDoc tags and changed some formats.

--- Sask.php
+++ Sask.php
@@ -1,10 +1,24 @@
 <?php
-
+/**
+ * Main class of the framework, initializes the application.
+ *
+ * @author David Summerton
+ * @author Stefanie Janine Stoelting <mail@stefanie-stoelting.de>
+ * @link http://saskphp.com/ Sask website
+ * @license http://opensource.org/licenses/MIT MIT
+ */
 class Sask
 {
 
-    static $database;
+    /**
+     * The reference to the database class.
+     * @var Database
+     */
+    public static $database;
 
+    /**
+     * Constructor of Sask, initialization of the database class.
+     */
     public function __construct()
     {
 
@@ -19,7 +33,12 @@ class Sask
         $database_password = "";
         $database_name = "";
 
-        $this->database = new Database($database_host, $database_user, $database_password, $database_name);
+        $this->database = new Database(
+                $database_host,
+                $database_user,
+                $database_password,
+                $database_name
+        );
     }
 
 }
@@ -27,13 +46,13 @@ class Sask
  * 	Global constants
  * 	==================
  * 	You need to set these constants.
- * 
+ *
  * 	Change SECURITYSALT to a random assortment of letters and numbers
  * 	as this is used in hashing.
- * 
+ *
  * 	Change BASE_DIR if Sask is not in the root URI directory.
  * 	Ensure to keep the beginning /
- * 
+ *
  * 	Change NOTFOUND_URI to whatever route/URI you wish to direct your
  * 	404 pages to.
  */
