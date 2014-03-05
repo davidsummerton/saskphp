e833fd6362efb883aa641520d514d6ae552ddb32
sjstoelting <mail@stefanie-stoelting.de>
2014-03-05 20:05:58 +0100

    Added a new class Configuration to have all configurations in one class available as static variables.

---
+++ Configurations/Configuration.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The configuration of the application. All settings should be set here.
+ *
+ * @author Stefanie Janine Stoelting<mail.stefanie-stoelting.de>
+ * @link http://saskphp.com/ Sask website
+ * @license http://opensource.org/licenses/MIT MIT
+ * @package Sask
+ */
+class Configuration
+{
+    /**
+     * The routes
+     * @var array
+     */
+    public static $routes = array(
+        '/' => 'Hello/world',
+    );
+}
