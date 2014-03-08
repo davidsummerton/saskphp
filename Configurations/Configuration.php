6ba39bab1d5d5215112603f91fbb857dfbf44cc6
sjstoelting <mail@stefanie-stoelting.de>
2014-03-08 15:45:25 +0100

    - Added constants for WEB_ROOT, which enables routing, if Saskphp is running in a subfolder of a domain. - Moved NOTFOUND_URI from class Sask to the Configuration - Added a database configuration array to support Saskphp without a database connection - Added some more examples to the routes variable

--- Configurations/Configuration.php
+++ Configurations/Configuration.php
@@ -9,11 +9,47 @@
  */
 class Configuration
 {
+
+    /**
+     * @var string Change SECURITYSALT to a random assortment of letters and
+     * numbers as this is used in hashing.
+     */
+    const SECURITYSALT = 'hfhy394niyn404983nuhirh65fv89uvd';
+
+    /**
+     * @var string The web root
+     */
+    const WEB_ROOT = '/';
+
+    /**
+     * @var string Change NOTFOUND_URI to whatever route/URI you wish to direct
+     * your 404 pages to.
+     */
+    const NOTFOUND_URI = '/404/';
+
+    /**
+     * The database connection informations
+     * @var array
+     */
+    public static $dbConnection = array(
+        'UseDatabase' => false,
+        'Type' => 'MySQL',
+        'Host' => 'localhost',
+        'User' => '',
+        'Password' => '',
+        'DatabaseName' => '',
+        'Port' => null,
+    );
+
     /**
      * The routes
      * @var array
      */
     public static $routes = array(
         '/' => 'Hello/world',
+        '/hello/world/' => 'Hello/world',
+        '/test/' => 'Hello/test',
+        '/404/' => 'Hello/page404',
     );
+
 }
